ca3a7e1debe4157c1e38ed2f741b7bed0f90e350
Gregory Gleason <gsgleason@gmail.com>
2021-06-08 13:22:45 -0600

    removing sslify

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
 from flask import Flask
-from flask_sslify import SSLify
 
 app = Flask(__name__)
 app.config.from_object('config.flask')
-sslify = SSLify(app, permanent=True)
 
 import db
 import index
